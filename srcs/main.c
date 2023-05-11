0fdd35c23c15704daf03a779bed839e3108c3745
Timothe Dutel <tdutel@student.42lyon.fr>
2023-05-11 16:25:00 +0200

    progress

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 11:09:31 by tdutel            #+#    #+#             */
-/*   Updated: 2023/05/11 12:39:59 by tdutel           ###   ########.fr       */
+/*   Updated: 2023/05/11 16:09:07 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,17 @@ static bool is_word(char *str, char *str2);
 // s = "echo"	 "-n"	 "lalalou"  ">"   "out" "NULL"
 
 
+
 t_token	*first_elem(char *str, char **envp)
 {
 	t_token	*new;
 	char	**s;
 	int		i;
-	int		j;
 	char	**path;
     char	*s_p;
+	char	*arg;
 
+	arg = NULL;
 	s = ft_split(str, ' ');
 	i = 0;
 	new = malloc(sizeof(t_token));
@@ -46,7 +48,7 @@ t_token	*first_elem(char *str, char **envp)
 		new->content = malloc(sizeof(char *) * 2);
 		if(!new->content)
 			return (NULL);
-		new->content[0] = strdup(s[0]);
+		new->content[0] = ft_strdup(s[0]);
 		new->content[1] = NULL;
 		if (s[0][1] == '<')
 			new->type = DOUBLE_CHEVRON_IN;
@@ -55,39 +57,123 @@ t_token	*first_elem(char *str, char **envp)
 	}
 	else if (is_builtin(s[0]) == true)
 	{
-		while (s[i + 1][0] == '-' )
+		while (s[i + 1] && s[i + 1][0] == '-' )
+		{
+			arg = ft_strjoin(arg, s[i + 1]);
 			i++;
-		j = 0;
-		new->content = malloc(sizeof(char *) * (2 + i));
+		}
+		new->content = malloc(sizeof(char *) * 2);
 		if(!new->content)
 			return (NULL);
-		new->content[0] = strdup(s[0]);
-		while (++j <= i)
-			new->content[j] = strdup(s[j]);
-		new->content[j] = NULL;
+		new->content[0] = ft_strdup(s[0]);
+		new->content[1] = ft_strdup(arg);
 		new->type = BUILTIN;
 	}
 	else
 	{
-		while (s[i + 1][0] == '-' )
+		while (s[i + 1] && s[i + 1][0] == '-' )
+		{
+			arg = ft_strjoin(arg, s[i + 1]);
 			i++;
-		j = 0;
+		}
 		path = get_path(envp);
         s_p = process(str, path);
         if (!s_p)
             return(NULL);
-		new->content = malloc(sizeof(char *) * (2 + i));
+		new->content = malloc(sizeof(char *) * (2));
 		if (!new->content)
 			return (NULL);
 		new->content[0] = ft_strdup(s_p);
-		while (++j <= i)
-			new->content[j] = strdup(s[j]);
-		new->content = NULL;
+		new->content[1] = ft_strdup(arg);
 		new->type = STRING;
 	}
 	new->next = NULL;
 	return (new);
 }
+
+
+t_token	*other_elem(char *str, char **envp, int	index)
+{
+	t_token	*new;
+	char	**s;
+	int		i;
+	char	**path;
+    char	*s_p;
+	char	*arg;
+
+	s = ft_split(str, ' ');
+	new = malloc(sizeof(t_token));
+	while(s[index][0] == '-')
+		index++;
+	if (s[index][0] == '>')
+	{
+		new->content = malloc(sizeof(char *) * 2);
+		if(!new->content)
+			return (NULL);
+		new->content[0] = ft_strdup(s[0]);
+		new->content[1] = NULL;
+		if (s[index][1] == '>')
+			new->type = DOUBLE_CHEVRON_IN;
+		else
+			new->type = CHEVRON_IN;
+	}
+	else if (is_builtin(s[index]) == true)
+	{
+		while (s[index + i + 1] && s[index + i + 1][0] == '-' )
+		{
+			arg = ft_strjoin(arg, s[index + i + 1]);
+			i++;
+		}
+		new->content = malloc(sizeof(char *) * 2);
+		if(!new->content)
+			return (NULL);
+		new->content[0] = ft_strdup(s[index]);
+		new->content[1] = ft_strdup(arg);
+		new->type = BUILTIN;
+	}
+	else
+	{
+		while (s[index + i + 1] && s[index + i + 1][0] == '-' )
+		{
+			arg = ft_strjoin(arg, s[index + i + 1]);
+			i++;
+		}
+		path = get_path(envp);
+        s_p = process(str, path);
+        if (!s_p)
+            return(NULL);
+		new->content = malloc(sizeof(char *) * (2));
+		if (!new->content)
+			return (NULL);
+		new->content[0] = ft_strdup(s_p);
+		new->content[1] = ft_strdup(arg);
+		new->type = STRING;
+	}
+	new->next = NULL;
+	return (new);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 static bool is_word(char *str, char *str2)
 {
@@ -115,6 +201,63 @@ static bool	is_builtin(char *str)
 		return (false);
 }
 
+
+
+
+
+
+
+
+
+
+t_token	*token_last(t_token *token)
+{
+	while (token && token->next)
+		token = token->next;
+	return (token);
+}
+
+void	token_add_back(t_token **token, t_token *new)
+{
+	t_token	*tail;
+
+	if (!token)
+		return ;
+	if (*token)
+	{
+		tail = token_last(*token);
+		tail->next = new;
+	}
+	else
+		*token = new;
+}
+
+
+
+
+
+t_token	*get_token(char *str, char **envp)
+{
+	t_token	*t_new;
+	t_token *tmp;
+	char	**s;
+	int		i;
+
+	i = 0;
+	s = ft_split(str, ' ');
+	t_new = first_elem(str, envp);
+	while (s[i++])
+	{
+		tmp = other_elem(str, envp, i);
+		token_add_back(&t_new, tmp);
+	}
+	return (t_new);
+}
+
+
+
+
+
 int main(int argc, char **argv, char **envp)
 {
     char    *str;
@@ -127,7 +270,8 @@ int main(int argc, char **argv, char **envp)
 	while(i < 5)
 	{
 		add_history(str);
-        first_elem(str, envp);
+		get_token(str, envp);
+        // first_elem(str, envp);
         i++;
         str = readline("Minishell$");
 	}
---
+++ srcs/path.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   path.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/10 12:19:20 by tdutel            #+#    #+#             */
+/*   Updated: 2023/05/11 12:51:49 by tdutel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+char	**get_path(char **envp)
+{
+	int		i;
+	char	**split_path;
+	char	*path;
+
+	i = 0;
+	while (ft_strnstr(envp[i], "PATH=", 5) == 0)
+		i++;
+	path = ft_substr(envp[i], 5, ft_strlen(envp[i]));
+	split_path = ft_split(path, ':');
+	free(path);
+	return (split_path);
+}
+
+char	**path_arg_cat(char **src, char *root_arg, char **argv)
+{
+	int		i;
+	char	**pathsrc;
+
+	pathsrc = malloc(sizeof(char *) * (5 + 3));
+	i = 0;
+	while (i < 5)
+	{
+		pathsrc[i] = ft_strjoin(src[i], root_arg);
+		i++;
+	}
+	pathsrc[i] = ft_strdup("libft/libft.a");
+	pathsrc[i + 1] = ft_strdup(argv[2]);
+	pathsrc[i + 2] = NULL;
+	return (pathsrc);
+}
+
+char	*process(char *str, char **path)
+{
+	char	**path_cmb;
+	int		i;
+	char	**split_argv;
+	char	*root_arg;
+
+	split_argv = ft_split(str, ' ');
+	root_arg = ft_strjoin("/", split_argv[0]);
+	path_cmb = path_arg_cat(path, root_arg, &str);
+	i = 0;
+	while (access(path_cmb[i], X_OK) == -1 && path_cmb[i])
+		i++;
+	if (!path_cmb[i])
+	{
+		return (NULL);
+	}
+	free(split_argv[0]);
+	split_argv[0] = path_cmb[i];
+	free(root_arg);
+	free(path_cmb);
+	return (split_argv[0]);
+}
