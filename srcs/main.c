6447866956e1171b1b92f299e90745df984fc7df
gd-harco <gd-harco@student.42lyon.fr>
2023-05-12 15:52:43 +0200

    perfected prompt, now work as intended

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 11:09:31 by tdutel            #+#    #+#             */
-/*   Updated: 2023/05/12 14:02:08 by gd-harco         ###   ########lyon.fr   */
+/*   Updated: 2023/05/12 15:52:19 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,31 @@ int	main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 	(void)envp;
+	printf("\033[0;31m");
+	printf("Bienvenue dans le Minishell de la Team Rocket!\n\n");
+	printf("##################################\n\
+####################################,\n\
+######################################\n\
+######################################,\n\
+###########               #############\n\
+###########               #############\n\
+######################################\n\
+#####################################*\n\
+####################################\n\
+#################################/\n\
+#################################/\n\
+###########           #############\n\
+###########            *############\n\
+###########              ############.\n\
+###########               #############\n\n");
+	printf("\033[0m");
 	str = get_user_input();
 	while (42)
 	{
-		printf("User input = \"%s\"\n", str);
-		if (ft_strncmp(str, "quit", 4) == 0)
+		if (ft_strncmp(str, "exit", 4) == 0)
 		{
 			free(str);
-			printf("La Team Rocket s'envole vers d'autres cieux!!!!\n");
+			printf("La Team Rocket s'envole vers d'autres cieux!!!!\nexit\n");
 			exit (0);
 		}
 		free(str);
