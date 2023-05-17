837c9efd6569b82787d84745699a78bd7402fce5
gd-harco <g.dharcourt+github@posteo.net>
2023-05-17 18:42:43 +0200

    count nb of pipe in token list

--- srcs/exec/execution.c
+++ srcs/exec/execution.c
@@ -6,15 +6,34 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:56:35 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/05/16 15:27:53 by gd-harco         ###   ########lyon.fr   */
+/*   Updated: 2023/05/17 18:40:22 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	get_nb_cmd(const t_minishell *data);
+
 void	master_exec(t_minishell *data)
 {
-	(void)data;
-	printf("TODO\n");
+	int	cmd_nb;
+
+	cmd_nb = get_nb_cmd(data);
+	ft_printf("cmd_nb = %d\n", cmd_nb);
 }
 
+static int	get_nb_cmd(const t_minishell *data)
+{
+	int		nb;
+	t_token	*tmp;
+
+	nb = 1;
+	tmp = data->token_list;
+	while (tmp)
+	{
+		if (tmp->type == PIPE)
+			nb++;
+		tmp = tmp->next;
+	}
+	return (nb);
+}
