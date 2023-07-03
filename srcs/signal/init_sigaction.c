76112a51781e8e56128c863425757a3fc04a0c40
gd-harco <gd-harco@student.42lyon.fr>
2023-07-03 22:55:46 +0200

    trop tard pour ces conneries

--- srcs/signal/init_sigaction.c
+++ srcs/signal/init_sigaction.c
@@ -12,9 +12,10 @@
 
 #include "minishell.h"
 
-static void	init_sig_int_prompt(struct sigaction *to_init);
-static void	init_siq_int_exec(struct sigaction *to_init);
-static void	init_sig_quit(struct sigaction *to_init);
+static void	init_sigint_parent(struct sigaction *to_init);
+static void	init_siqint_child(struct sigaction *to_init);
+static void	init_sigquit_parent(struct sigaction *to_init);
+static void	init_sigquit_child(struct sigaction *to_init);
 
 void	init_sigaction(t_sig *sig)
 {
@@ -22,28 +23,39 @@ void	init_sigaction(t_sig *sig)
 	sig->int_exec = malloc(sizeof(struct sigaction));
 	sig->quit_prompt = malloc(sizeof(struct sigaction));
 	sig->quit_exec = malloc(sizeof(struct sigaction));
-	init_siq_int_exec(sig->int_exec);
-	init_sig_int_prompt(sig->int_prompt);
-	init_sig_quit_prompt(sig->quit_prompt);
-	init_siq_quit_exec(sig->quit_exec);
+	init_siqint_child(sig->int_exec);
+	init_sigint_parent(sig->int_prompt);
+	init_sigquit_parent(sig->quit_prompt);
+	init_siqquit_child(sig->quit_exec);
 }
 
-//ctrl+\ dans le prompt
-static void	init_sig_quit(struct sigaction *to_init)
+//ctrl + c dans un process enfant kill le process
+static void	init_siqint_child(struct sigaction *to_init)
 {
 	to_init->sa_handler = SIG_IGN;
 	sigemptyset(&to_init->sa_mask);
 	to_init->sa_flags = 0;
 }
 
-static void	init_siq_int_exec(struct sigaction *to_init)
+//ctrl + c dams un process parent affiche le prompt sur une nouvelle ligne
+static void	init_siqint_parent(struct sigaction *to_init)
 {
 	to_init->sa_handler = SIG_IGN;
 	sigemptyset(&to_init->sa_mask);
 	to_init->sa_flags = 0;
 }
 
-static void	init_sig_int_prompt(struct sigaction *to_init)
+//ctrl+\ dans le prompt ne fais rien
+static void	init_sigquit_parent(struct sigaction *to_init)
+{
+	to_init->sa_handler = SIG_IGN;
+	sigemptyset(&to_init->sa_mask);
+	to_init->sa_flags = 0;
+}
+
+
+// ctrl + \ dans un ori
+static void	init_sigint_parent(struct sigaction *to_init)
 {
 	to_init->sa_handler = sig_c_prompt;
 	sigemptyset(&to_init->sa_mask);
