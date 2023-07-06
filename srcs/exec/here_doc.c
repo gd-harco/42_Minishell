8288986de60570679b3186451a2447ad085d334a
gd-harco <gd-harco@student.42lyon.fr>
2023-07-06 13:13:41 +0200

    fixed "ctrl+d" behaviour in here doc

--- srcs/exec/here_doc.c
+++ srcs/exec/here_doc.c
@@ -99,11 +99,22 @@ static void	write_in_here_doc(t_here_doc *here_doc, size_t nb_here_doc)
 		here_doc[i].tmp_char = readline(HD_PROMPT);
 		while (ft_strcmp(here_doc[i].tmp_char, here_doc[i].delimiter) != 0)
 		{
-			write(here_doc[i].pipe_fd[1], here_doc[i].tmp_char,
-				ft_strlen(here_doc[i].tmp_char));
-			free(here_doc[i].tmp_char);
-			write(here_doc[i].pipe_fd[1], "\n", 1);
-			here_doc[i].tmp_char = readline(HD_PROMPT);
+			if (here_doc[i].tmp_char == NULL)
+			{
+				free(here_doc[i].tmp_char);
+				write(here_doc[i].pipe_fd[1], "\n", 1);
+				ft_dprintf(2, HERE_DOC_EOF " (wanted `%s')\n",
+					here_doc[i].delimiter);
+				break ;
+			}
+			else
+			{
+				write(here_doc[i].pipe_fd[1], here_doc[i].tmp_char,
+					ft_strlen(here_doc[i].tmp_char));
+				free(here_doc[i].tmp_char);
+				write(here_doc[i].pipe_fd[1], "\n", 1);
+				here_doc[i].tmp_char = readline(HD_PROMPT);
+			}
 		}
 		free(here_doc[i].tmp_char);
 		close(here_doc[i].pipe_fd[1]);
