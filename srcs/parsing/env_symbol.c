fd4c83da7fa6254b74f84ae602a4ab3ea45a4abe
Timothe Dutel <tdutel@student.42lyon.fr>
2023-07-11 13:56:45 +0200

    fixed leaks for return_value in env_symbol

--- srcs/parsing/env_symbol.c
+++ srcs/parsing/env_symbol.c
@@ -6,7 +6,7 @@
 /*   By: tdutel <tdutel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 16:22:41 by tdutel            #+#    #+#             */
-/*   Updated: 2023/07/10 18:39:59 by tdutel           ###   ########.fr       */
+/*   Updated: 2023/07/11 13:55:16 by tdutel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,16 @@
 
 int	quote_env_symbol(t_var *var, char *str_tmp, t_varenv *v_e)
 {
+	char	*ret_val;
+
 	if (str_tmp[v_e->i + 1 + v_e->k] == '?')
 	{
 		if (str_tmp[v_e->i + v_e->k] == '$')
-			var->env = ft_strjoinsp(var->env, ft_itoa(g_return_value), 0);
+		{
+			ret_val = ft_itoa(g_return_value);
+			var->env = ft_strjoinsp(var->env, ret_val, 0);
+			free(ret_val);
+		}
 		else
 			return (0);
 	}
@@ -32,11 +38,15 @@ int	quote_env_symbol(t_var *var, char *str_tmp, t_varenv *v_e)
 
 int	env_symbol(t_var *var, char *str, t_varenv *v_e)
 {
+	char	*ret_val;
+
 	if (str[v_e->i + 1 + v_e->k] == '?')
 	{
 		if (var->s[v_e->j][v_e->i + v_e->k] == '$')
 		{
-			var->env = ft_strjoinsp(var->env, ft_itoa(g_return_value), 0);
+			ret_val = ft_itoa(g_return_value);
+			var->env = ft_strjoinsp(var->env, ret_val, 0);
+			free(ret_val);
 		}
 		else
 			return (0);
