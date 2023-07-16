c6e0093a92f4a69619732a3e0ec9cddc35fcf7f6
gd-harco <gd-harco@student.42lyon.fr>
2023-07-16 21:09:00 +0200

    cleanup

--- valgrind_cmd.c
+++
@@ -1,4 +0,0 @@
-/*
-valgrind --suppressions=ignore_guillaume_leaks.txt --log-file=valgrind.log --leak-check=full --show-leak-kinds=all ./minishell
-valgrind --suppressions=ignore_rl_leaks.txt --log-file=valgrind.log --leak-check=full --show-leak-kinds=all ./minishell
-*/
